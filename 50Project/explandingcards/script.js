6036046d85073b1afe6e5765bd5699cfeed15d07
AyushGupta11442 <ayushgupta11442@gmail.com>
2025-08-16 19:35:28 +0530

    day 1 done

--- 50Project/explandingcards/script.js
+++ 50Project/explandingcards/script.js
@@ -1,15 +1,14 @@
-const panels = document.querySelectorAll('.pannel')
+const panels = document.querySelectorAll('.pannel');
 
-
-panels.array.forEach((panel) => {
-    panel.addEventListner('click' , ()=>{
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
 });
 
-function removeActiveClasses(params) {
-    panel.forEach(panel =>{
-        panel.classList.remove('active')
-    })
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
 }
